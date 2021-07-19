7f1cd937da61260e4a49ed6dcd6474f62be43ddb
Richard Howard <richard@promptworks.com>
2021-07-19 14:05:43 -0400

    Switch to sonokai maia

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -1,4 +1,4 @@
-vim.g.sonokai_style = "andromeda"
+vim.g.sonokai_style = "maia"
 vim.g.sonokai_enable_italic = 1
 vim.g.sonokai_disable_italic_comment = 1
 vim.g.sonokai_diagnostic_virtual_text = "colored"
