98982824ed8d8e6d270628fd5b15a4b18e762497
Richard Howard <richard.howard@wonderschool.com>
2024-10-23 21:31:57 -0400

    Fix nvim keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,31 +3,9 @@
 
 local wk = require("which-key")
 
-wk.register({
-  ["<leader>w"] = {
-    ["w"] = { "<C-W>p", "Other window" },
-    ["d"] = { "<C-W>c", "Delete window" },
-    [","] = { "<C-W>v", "Split window right" },
-    ["."] = { "<C-W>s", "Split window down" },
-    ["2"] = { "<C-W>v", "Double column layout" },
-    ["h"] = { "<C-W>h", "Window left" },
-    ["j"] = { "<C-W>j", "Window below" },
-    ["l"] = { "<C-W>l", "Window right" },
-    ["k"] = { "<C-W>k", "Window up" },
-    ["H"] = { "<C-W>5<", "Expand window left" },
-    ["J"] = { ":resize +5", "Expand window below" },
-    ["L"] = { "<C-W>5>", "Expand window right" },
-    ["K"] = { ":resize -5", "Expand window up" },
-    ["="] = { "<C-W>=", "Balance window" },
-    ["s"] = { "<C-W>s", "Split window below" },
-    ["v"] = { "<C-W>v", "Split window right" },
-  },
-  ["<leader>b"] = {
-    ["p"] = { ":bp<CR>", "Previous" },
-  },
-  ["<leader>g"] = {
-    ["g"] = { "<cmd>Neogit<cr>", "NeoGit" },
-    ["G"] = { "<cmd>Neogit kind=split<cr>", "NeoGit split" },
-  },
-  ["<leader>t"] = { "<cmd>ToggleTerm<cr>", "Toggle terminal" },
+wk.add({
+  { "<leader>gg", "<cmd>Neogit<cr>", desc = "NeoGit" },
+  { "<leader>gG", "<cmd>Neogit kind=split<cr>", desc = "NeoGit split" },
+
+  { "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
 })
