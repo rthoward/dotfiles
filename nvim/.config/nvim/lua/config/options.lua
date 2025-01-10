130c81e60ffe502d3a66bfc7dc6bf3adff43b124
Richard Howard <richard.howard@wonderschool.com>
2025-01-10 14:16:29 -0500

    Set nvim background based on system theme

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,6 +4,18 @@
 
 local opt = vim.opt
 
-opt.background = "dark"
 vim.g.maplocalleader = ","
 opt.relativenumber = false
+
+function get_system_theme()
+  local handle = io.popen('defaults read -g AppleInterfaceStyle 2>/dev/null || echo "Light"')
+  local result = handle:read("*a")
+  handle:close()
+  return result:gsub("^%s*(.-)%s*$", "%1") -- Trim whitespace
+end
+
+if get_system_theme() == "Light" then
+  vim.o.background = "light"
+else
+  vim.o.background = "dark"
+end
